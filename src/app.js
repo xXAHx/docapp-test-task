ead602241c3b9307c9fbe30d86c217cf7ec6359a
nickolamaz <nickolay.ma@gmail.com>
2019-06-11 09:11:44 -0400

    Init repository

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// this is an example of improting data from JSON
+import rooms from '../data/rooms.json';
+
+export default (function () {
+  // YOUR CODE GOES HERE
+  // next line is for example only
+  document.getElementById("app").innerHTML = "<h1>Hello from Docapp</h1>";
+}());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    open: true,
+    port: 9000,
+    before: function(app, server) {
+      app.use("/api", express.static(path.join(__dirname, "data")));
+    }
+  }
+};
